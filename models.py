a8074027085b1a2d3fe44295bb3342986c4d8d2f
David Parsley <davidparsley.kakhayanga@gmail.com>
2025-01-18 00:02:10 +0300

    populates models.py

---
+++ models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+metadata = MetaData()
+
+db = SQLAlchemy(metadata=metadata)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(128), nullable=False)
+    last_name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    loans = db.relationship('Loan', backref='user', lazy=True)
+
+
+class Loan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer, nullable=False)
+    interest_rate = db.Column(db.Numeric(10, 2), nullable=False)
+    start_date = db.Column(db.Date, nullable=False)  
+    loan_status = db.Column(db.String(20), default='Active', nullable=False) 
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
