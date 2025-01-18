b3ebd72ea5d24d7acd18500041130b03a0027afc
David Parsley <davidparsley.kakhayanga@gmail.com>
2025-01-18 21:26:30 +0300

    configures jwt in app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask_migrate import Migrate
 from models import  db
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
+
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bank.db'
 migrate = Migrate(app, db)
 db.init_app(app)
+
+app.config["JWT_SECRET_KEY"] = "dgjhjhgjllijikmuh"  
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30)
+
+jwt = JWTManager(app)
+jwt.init_app(app)
+
 
 from views import *
 
